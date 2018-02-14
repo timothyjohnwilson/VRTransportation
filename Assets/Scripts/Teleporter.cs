5ce1c23ce7d80c0fa317e277b700d4d250ecd0b6
Timothy Wilson <blaare@protonmail.com>
2018-02-14 00:18:17 -0500

    Created Teleportation Script!

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+
+
+	public GameObject TeleportMarker;
+	public Transform Controller;
+	public float RayLength = 500f;
+	public TeleporterLine teleporterLine;
+	private bool pressed;
+	private CharacterController characterController;
+
+	// Use this for initialization
+	void Start () {
+		//Map the Device from user input
+		TeleportMarker.SetActive (false);
+		teleporterLine.initialize ();
+		pressed = false;
+		characterController = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = new Ray (Controller.transform.position, Controller.transform.forward);
+		TeleporterController (ray);
+	}
+
+	void TeleporterController(Ray ray){
+		RaycastHit hit;
+		bool detected = Physics.Raycast (ray, out hit, RayLength);
+		Vector3 markerPosition = TeleportMarker.transform.position;
+		//Handle when the user presses 'A'
+		if (OVRInput.GetDown (OVRInput.RawButton.A)) {
+			pressed = true;
+		} else if (OVRInput.GetUp (OVRInput.RawButton.A)) {
+			pressed = false;
+			teleporterLine.Hide ();
+			if(hit.collider.tag == "Ground")
+				transform.position = new Vector3(hit.point.x, hit.point.y + characterController.height, hit.point.z);
+		}
+		//Line Handling
+		if (pressed) {
+			if (detected && hit.collider.tag == "Ground") {
+				teleporterLine.updateColor (Color.green, Color.green);
+				TeleportMarker.SetActive (true);
+				TeleportMarker.transform.position = hit.point;
+				teleporterLine.Display (TeleportMarker.transform.position);
+			} else {
+				teleporterLine.updateColor (Color.red, Color.red);
+				TeleportMarker.SetActive (false);
+				Ray ray2 = new Ray (Controller.transform.position, Controller.transform.forward);
+				teleporterLine.Display (ray2.GetPoint(RayLength));
+			}
+
+		} else {
+			
+			TeleportMarker.SetActive (false);
+		}
+			
+	}
+		
+}
---
+++ Assets/Scripts/TeleporterLine.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleporterLine : MonoBehaviour {
+
+	// Creates a line renderer that follows a Sin() function
+	// and animates it.
+
+	public Color c1 = Color.red;
+	public Color c2 = Color.red;
+	public GameObject Teleporter;
+	public GameObject hand;
+
+	public void initialize()
+	{
+		LineRenderer lineRenderer = gameObject.AddComponent<LineRenderer>();
+		lineRenderer.material = new Material(Shader.Find("Particles/Additive"));
+		lineRenderer.widthMultiplier = 0.05f;
+		lineRenderer.positionCount = 2;
+
+		// A simple 2 color gradient with a fixed alpha of 1.0f.
+		float alpha = 1.0f;
+		Gradient gradient = new Gradient();
+		gradient.SetKeys(
+			new GradientColorKey[] { new GradientColorKey(c1, 0.0f), new GradientColorKey(c2, 1.0f) },
+			new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(alpha, 1.0f) }
+		);
+		lineRenderer.colorGradient = gradient;
+	}
+
+	public void Display(Vector3 teleportPosition)
+	{
+		LineRenderer lineRenderer = GetComponent<LineRenderer>();
+		var t = Time.time;
+		Debug.Log (hand.transform.position);
+		Debug.Log (Teleporter.transform.position);
+		Vector3 controllerPosition = new Vector3 (hand.transform.position.x, hand.transform.position.y, hand.transform.position.z);
+		Vector3[] positions = { controllerPosition, teleportPosition };
+		lineRenderer.SetPositions(positions);
+	}
+
+	public void Hide(){
+		LineRenderer lineRenderer = GetComponent<LineRenderer>();
+		var t = Time.time;
+		Vector3 controllerPosition = new Vector3 (0, 0, 0);
+		Vector3 teleporterPosition = new Vector3 (0, 0, 0);
+		Vector3[] positions = { controllerPosition, teleporterPosition };
+		lineRenderer.SetPositions(positions);
+	}
+
+	public void updateColor(Color c1, Color c2){
+		LineRenderer lineRenderer = GetComponent<LineRenderer> ();
+		Gradient gradient = new Gradient();
+		float alpha = 1.0f;
+		gradient.SetKeys(
+			new GradientColorKey[] { new GradientColorKey(c1, 0.0f), new GradientColorKey(c2, 1.0f) },
+			new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(alpha, 1.0f) }
+		);
+		lineRenderer.colorGradient = gradient;
+
+	}
+}
